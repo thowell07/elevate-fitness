fd783ae0dea81810d0e04881a078cb9f001d63f3
thowell07 <tarae.howell@gmail.com>
2026-05-25 09:07:50 -0400

    Bump PWA shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'elevate-shell-v1';
+const CACHE_NAME = 'elevate-shell-v2';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/elevate-icon.svg'];
 
 self.addEventListener('install', (event) => {
