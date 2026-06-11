3aee135df52b59f92147f991bb7727bbebbdfba4
thowell07 <tarae.howell@gmail.com>
2026-06-11 19:00:00 -0400

    Simplify workout type names

--- src/lib/workoutDetails.js
+++ src/lib/workoutDetails.js
@@ -1,6 +1,6 @@
-export const DEFAULT_WORKOUT_TYPE = 'Lifetime Gym';
-export const CROSSFIT_WORKOUT_TYPE = 'CrossFit At Home Workout';
-export const AT_HOME_WORKOUT_TYPE = 'Just At-Home Workout';
+export const DEFAULT_WORKOUT_TYPE = 'Lifetime';
+export const CROSSFIT_WORKOUT_TYPE = 'CrossFit';
+export const AT_HOME_WORKOUT_TYPE = 'At Home';
 
 export const WORKOUT_TYPE_OPTIONS = [
   DEFAULT_WORKOUT_TYPE,
@@ -11,11 +11,19 @@ export const WORKOUT_TYPE_OPTIONS = [
 const WORKOUT_DETAILS_KIND = 'workoutDetails';
 const WORKOUT_DETAILS_ID = 'workout-details';
 const FREEFORM_WORKOUT_TYPES = new Set([CROSSFIT_WORKOUT_TYPE, AT_HOME_WORKOUT_TYPE]);
+const LEGACY_WORKOUT_TYPE_MAP = {
+  'Lifetime Gym': DEFAULT_WORKOUT_TYPE,
+  'CrossFit At Home Workout': CROSSFIT_WORKOUT_TYPE,
+  'Just At-Home Workout': AT_HOME_WORKOUT_TYPE,
+  'Lifetime Gym and At Home Workout': AT_HOME_WORKOUT_TYPE,
+};
 
-export const isCrossFitWorkout = (workoutType) => workoutType === CROSSFIT_WORKOUT_TYPE;
+export const isCrossFitWorkout = (workoutType) => normalizeWorkoutType(workoutType) === CROSSFIT_WORKOUT_TYPE;
 
-export const normalizeWorkoutType = (value) =>
-  WORKOUT_TYPE_OPTIONS.includes(value) ? value : DEFAULT_WORKOUT_TYPE;
+export const normalizeWorkoutType = (value) => {
+  if (WORKOUT_TYPE_OPTIONS.includes(value)) return value;
+  return LEGACY_WORKOUT_TYPE_MAP[value] || DEFAULT_WORKOUT_TYPE;
+};
 
 export const isFreeformWorkout = (workoutType) => FREEFORM_WORKOUT_TYPES.has(normalizeWorkoutType(workoutType));
 
